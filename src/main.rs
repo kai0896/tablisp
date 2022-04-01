e7c2b58d9b31e4ff19712946126a356564f02664
Kai Haehnle <kh120@hdm-stuttgart.de>
2022-04-01 13:07:03 +0200

    added saving to file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
+use std::env;
 use std::fs;
 pub mod calc;
 
 fn main() {
-    let path = "../test.csv";
-    let contents = fs::read_to_string(path)
-        .expect("Something went wrong reading the file");
+    let args: Vec<String> = env::args().collect();
+    match args.get(1) {
+        Some(path) => {
+            let contents = fs::read_to_string(path)
+                .expect("Something went wrong reading the file");
 
-    let cells = parse_csv(contents);
-    let new_cells = calc::lookup_cell_references(cells);
+            let cells = parse_csv(contents);
+            let new_cells = calc::lookup_cell_references(cells);
 
-    // let new_csv = make_csv(new_cells);
-    println!("new cells: {:?}", new_cells);
+            let new_csv = make_csv(new_cells);
+            print!("new cells: \n\n{}", new_csv);
+
+            let mut save_path = path.clone();
+            save_path.insert_str(path.len()-4, "_calced");
+            fs::write(save_path, new_csv).expect("Unable to write file");
+
+        }
+        None => println!("Error: No file path provided as argument!")
+    }
 }
 
 fn parse_csv(contents: String) -> Vec<Vec<String>> {
@@ -28,6 +39,17 @@ fn parse_csv(contents: String) -> Vec<Vec<String>> {
 }
 
 fn make_csv(cells: Vec<Vec<String>>) -> String {
-    todo!()
+    let mut res = String::new();
+    for i in 0..cells.len(){
+        for j in 0..cells[i].len(){
+            let cell = cells[i][j].clone();
+            res.push_str(cell.as_str());
+            res.push_str(", ");
 
+        }
+        res.pop();
+        res.pop();
+        res.push('\n')
+    }
+    res
 }
